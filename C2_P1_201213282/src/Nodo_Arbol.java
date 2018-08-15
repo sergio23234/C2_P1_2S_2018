1852a955d33b84cb68cfcfe7724a2dc01ee97249
Sergio De los Rios <sergiofernando23234@gmail.com>
2018-08-15 11:34:04 -0600

    subir
    subir

---
+++ C2_P1_201213282/src/Nodo_Arbol.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sergi
+ */
+public class Nodo_Arbol {
+ String etiqueta ;
+ String ID;
+ String tipo;
+ String sugerir;
+ String restringirmsn;
+ String requerido;
+ String Predeterminado;
+ String Codigo_pre;
+ String codigo_post;
+    public Nodo_Arbol()
+ {
+     etiqueta ="";
+     ID ="";
+     tipo ="";
+     sugerir="";
+     restringirmsn="";
+     requerido="";
+     Predeterminado="";
+     Codigo_pre="";
+     codigo_post="";
+ }   
+}
